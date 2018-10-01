197fd52f851c3f14f8c1415a1cb46eb7bf9b5fa7
ShantnuS <shantnu@outlook.com>
2018-10-01 16:09:19 +0100

    minor tweaks

--- weasel.py
+++ weasel.py
@@ -1,7 +1,6 @@
 import random
 
-size = 28
-target = "methinks it is like a weasel"
+target = "methinks it is like a weasel" #This is what the individuals aspire to be
 
 def list_to_string(list_to_convert):
     return ''.join(list_to_convert)
@@ -11,13 +10,16 @@ def string_to_list(string_to_convert):
 
 def create_individual(size):
     individual = [] 
-    for index in range(size):
+    for _ in range(size):
         ascii_index = random.randint(32,127)
         individual.append(str(chr(ascii_index)))
     print("Created the individual: " + list_to_string(individual))    
     return individual
 
 def main():
+    size = len(target) #This is how long the individuals will be 
+
+    #Create some individuals
     individual1 = create_individual(size)
     individual2 = create_individual(size)
 
