7c717e10b0b5fd631d3f97f92ba42a64e52ece10
ShantnuS <shantnu@outlook.com>
2018-10-08 16:13:39 +0100

    Mutation hill climber

--- weasel.py
+++ weasel.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 target = "methinks it is like a weasel" #This is what the individuals aspire to be
 
@@ -22,18 +23,53 @@ def calculate_fitness(individual):
             fitness += 1
     return fitness
 
+def mutate_individual(individual, size):
+    child = []
+    for index, item in enumerate(individual):
+        random_value = random.uniform(0,1)
+        if random_value < 1/size:
+            mutation = str(chr(random.randint(32,126)))
+            child.append(mutation)
+        else:
+            child.append(item)
+    return child
+
+def run_mutation_hill_clumber(size):
+    individual_A = create_individual(size)
+    fitness_A = calculate_fitness(individual_A)
+
+    while(fitness_A != size):
+        individual_B = mutate_individual(individual_A, size)
+        # print(list_to_string(individual_B))
+        fitness_A = calculate_fitness(individual_A)
+        fitness_B = calculate_fitness(individual_B)
+        if fitness_B > fitness_A: 
+            individual_A = individual_B
+            fitness_A = fitness_B
+
+        print(list_to_string(individual_A) + " = " +str(fitness_A))
+
 def main():
     size = len(target) #This is how long the individuals will be 
 
-    #Create some individuals
-    individual1 = create_individual(size)
-    individual2 = create_individual(size)
+    start = int(round(time.time() * 1000))
 
-    print(calculate_fitness(individual1))
+    run_mutation_hill_clumber(size)
+    
+    end = int(round(time.time() * 1000))
+    time_taken = end - start
+    print("The run took %dms" % time_taken)
 
-    '''
-    for _ in range(100):
-        print(calculate_fitness(create_individual(size)))
-    '''
+    # #Create some individuals
+    # individual1 = create_individual(size)
+    # individual2 = create_individual(size)
+
+    # print(calculate_fitness(individual1))
+
+    # print("Mutating 100 times")
+    # for _ in range(100):
+    #     individual1 = mutate_individual(individual1, size)
+    #     print(list_to_string(individual1) + " " + str(calculate_fitness(individual1)))
+    
 
 if __name__ == "__main__": main()
